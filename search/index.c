11947afd5c4e6209550006506bd6e95d35bd7752
blossomblo0m <161424592+blossomblo0m@users.noreply.github.com>
2024-04-23 11:51:35 +0300

    Update index.c

---
+++ search/index.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/headers/trie.h"
+#include "../lib/headers/unordered_set.h"
+#include "../lib/headers/unordered_map.h"
+
+int compare(const void * x1, const void * x2) {
+    int *tmp1 = (int*)x1, *tmp2 = (int*)x2;
+    int a = tmp1[0] - tmp2[0], b = tmp1[1] - tmp2[1];
+    return a > 0 || (a == 0 && b < 0);
+}
+
+void
+main(int argc, char **argv) {
+    FILE *out;
+    out = fopen("index.txt", "w");
+    fprintf(out, "%d\n", argc - 1);
+
+    int **name_size = calloc(argc - 1, sizeof(*name_size)); //[размер][имя]
+    int *tmp = calloc((argc - 1) * 2, sizeof (*tmp));
+    for (int i = 0; i < argc - 1; ++i) {
+        name_size[i] = &tmp[i * 2];
+    }
+
+    for (int i = 1; i < argc; i++) {
+        FILE *cur_file;
+        cur_file = fopen(argv[i], "r"); //файл с названием argv[i]
+        
+        int file_size = 0;
+        while(getc(cur_file) != EOF) // количество элементов
+            file_size++;
+        fclose(cur_file);
+
+        name_size[i - 1][1] = i;
+        name_size[i - 1][0] = file_size;
+    }
+
+    qsort(name_size, argc - 1, sizeof(*name_size), compare); //сортируем по убыванию размера
+
+    Trie words_in_files = t_init();
+    // UnorderedMap mp;
+    for (int i = 0; i < argc - 1; i++) { //присваивание файлам с большим размером меньший номер
+        fprintf(out, "%s %d\n", argv[name_size[i][1]], i);
+       // um_insert(&mp, argv[name_size[i][1]], (char*)i);
+    }
+
+    UnorderedSet dictionary;
+    us_init(&dictionary);
+    for (int i = 0; i < argc - 1; i++) {
+        FILE *cur_file;
+        cur_file = fopen(argv[name_size[i][1]], "r");
+
+        char *word;
+        while (fscanf(cur_file, "%s", word) == 1) { //добавление слов в бор и сет
+            t_push_back(&words_in_files, word, i);
+            us_insert(&dictionary, word);
+        }
+        
+        fclose(cur_file);
+    }
+
+    um_free(&dictionary);
+    t_free(&words_in_files);
+    free(name_size);
+    free(tmp);
+    fclose(out);
+    free(tmp);
+    free(name_size);
+}
